be1827827503416a1eafd56ca99f785995f682d3
Travis Poulsen <travis.poulsen@gmail.com>
2017-08-22 14:17:39 -0400

    Bump version number => 0.3.4

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Focus.Mixfile do
 
   def project do
     [app: :focus,
-     version: "0.3.3",
+     version: "0.3.4",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      consolidate_protocols: Mix.env != :test,
